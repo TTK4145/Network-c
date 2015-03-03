281f57789f8270506b327af8c6dbad1f5278290d
itk-d674 <kyrreerl@itk-d651>
2015-03-03 13:40:12 +0100

    fixed syntax and funskjons

--- tcp/tcp.c
+++ tcp/tcp.c
@@ -23,11 +23,20 @@ struct listener_info{
 };
 
 struct hash_node {
-	int * next;
-	struct tcp_connection data;
+	struct hash_node * next;
+	struct tcp_connection * data;
 };
 
-struct hash_node* connection_hash [255] = {.[0 .. 255] = (struct hash_node*){.next = NULL, .data = (struct tcp_connection)NULL}}; 
+struct hash_node empty_node = {.next = NULL, .data = NULL};
+static struct hash_node * connection_hash[256]={&empty_node};
+
+//Foreward declared fucntions
+void  * tcp_connection_server(void * data);
+int add_conn(int newSock, struct sockaddr_in raddr);
+
+int remove_conn(struct sockaddr_in raddr);
+
+int ip_to_last_subnet_int (char * ip);
 
 
 void error(char * msg)
@@ -40,29 +49,29 @@ void tcp_init(int localListenPort, int maxPending)
 {
 	pthread_t conn_server;
 	struct listener_info l_info = {.port=localListenPort, .maxPending = maxPending };
-	pthread_create(&conn_server, NULL, tcp_connection_server, (void *) l_info);
+	pthread_create(&conn_server, NULL, tcp_connection_server, (void *) &l_info);
 	
 	
 	
 	
 }
 
-void tcp_connection_server(void * data){
+void * tcp_connection_server(void * data){
 	struct listener_info l_info = *(struct listener_info *)data;
 	struct sockaddr_in laddr, raddr;
 	unsigned int raddrLen;
 	int listenSock;
 
 	//local 
-	lsocket = socket(PF_INET, SOCK_STREAM, 0);
-	if (lsocket < 0) {
+	listenSock = socket(PF_INET, SOCK_STREAM, 0);
+	if (listenSock < 0) {
 		error("ERROR opening local socket");
 	}
 	memset(&laddr, 0 , sizeof(laddr));
 	laddr.sin_family = AF_INET;
-	laddr.sin_port = htons(l_info.localListenPort);
+	laddr.sin_port = htons(l_info.port);
 	laddr.sin_addr.s_addr = INADDR_ANY; 
-	if (bind(lsocket, (struct sockaddr*) &laddr, sizeof(laddr)) < 0)
+	if (bind(listenSock, (struct sockaddr*) &laddr, sizeof(laddr)) < 0)
 	{
 		error("ERROR on binding local socket");
 	}
@@ -76,8 +85,8 @@ void tcp_connection_server(void * data){
 	for (;;){
 		memset(&raddr, 0 , sizeof(raddr));
 		raddrLen = sizeof(raddr);
-		int newSock = accept(listenSock, &raddr, &raddrLen);
-		add(newSock, raddr);
+		int newSock = accept(listenSock, (struct sockaddr *)&raddr, &raddrLen);
+		add_conn(newSock, raddr);
 	}
 
 }
@@ -91,13 +100,14 @@ void tcp_close_all(){
 
 }
 
-int remove(struct sockaddr_in raddr){
+int remove_conn(struct sockaddr_in raddr){
 	struct hash_node * temp;
 	
 	int i = ip_to_last_subnet_int(inet_ntoa(raddr.sin_addr));
 	int port = ntohs(raddr.sin_port);
+	temp = connection_hash[i];
 	while (temp->next != NULL){
-		if (ntohs(temp->next->data->addr) != port){
+		if (ntohs(temp->next->data->addr.sin_port) != port){
 			
 			close(temp->data->sock);
 			
@@ -107,24 +117,21 @@ int remove(struct sockaddr_in raddr){
 		}
 	}
 	return -1;
-
-
-
 }
 
-void add(int newSock, struct sockaddr_in raddr){
+int add_conn(int newSock, struct sockaddr_in raddr){
 	// PORT int i = ntohs(raddr.sin_port);
 	struct hash_node * new = (struct hash_node *) malloc(sizeof(struct hash_node));
-	new.next = NULL;
-	new.tcp_connection = (tcp_connection){.addr = raddr, .sock = newSock};
+	new->next = NULL;
+	new->data = (tcp_connection){.addr = raddr, .sock = newSock};
 	
 	int i = ip_to_last_subnet_int(inet_ntoa(raddr.sin_addr));
 	
 	struct hash_node * temp = connection_hash[i];
-	while (temp.next != NULL){
-		temp = temp.next;
+	while (temp->next != NULL){
+		temp = temp->next;
 	} 
-	temp.next = new;
+	temp->next = new;
 }
 
 int ip_to_last_subnet_int (char * ip) {
